f2a11e8583cf526e3da10d052c484bb7b2fa7e2e
marco <acermarco95@gmail.com>
2016-04-30 00:17:07 +0200

    inserite due ultime classi per la decriptazione

--- SistemiOperativi/src/it/sisop1516/crypto/CryptoUtils.java
+++ SistemiOperativi/src/it/sisop1516/crypto/CryptoUtils.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -47,18 +48,21 @@ public class CryptoUtils {
 	{
 		doCrypto(Cipher.DECRYPT_MODE,key,inputFile,outputFile);
 	}//decrypt(byte[])
-	public static void decryptSISOP(String key,byte [] encrypted,byte[] output) throws CryptoException{
-		doCrypto(Cipher.DECRYPT_MODE,key,encrypted,output);
+	
+	public static String decrypt(String key,byte [] encrypted) throws CryptoException{
+		return doCrypto(Cipher.DECRYPT_MODE,key,encrypted);
 	}
-	private static void doCrypto(int cipherMode,String key,byte[] input,byte[] output) throws CryptoException {
+	
+	private static String doCrypto(int cipherMode,String key,byte[] input)  {
 		try{
+			//creo un oggetto chiave a partire dalla stringa di riferimento
 			Key secretKey=new SecretKeySpec(key.getBytes(),ALGORITHM);
 			Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(cipherMode, secretKey);
-            output=cipher.doFinal(input);
+            cipher.init(cipherMode, secretKey); //inizializzo il cipher con il relativo algoritmo crittografico
+            return new String(cipher.doFinal(input));
             }catch(NoSuchPaddingException | NoSuchAlgorithmException
                 | InvalidKeyException | BadPaddingException
-                | IllegalBlockSizeException ex){/*throw new CryptoException("Error encrypting/decrypting file", ex);*/}
+                | IllegalBlockSizeException ex){return "";}
 	}
 	
 	private static void doCrypto(int cipherMode, byte[] b, File inputFile, File outputFile) throws CryptoException {
@@ -119,5 +123,29 @@ public class CryptoUtils {
 		
 	}//doCrypto
 	
-
+	public static void main(String [] args) throws IOException, CryptoException{
+		File f=new File("/home/marco/informatic_probs.txt");
+		File enc=new File("/home/marco/documentoImportante.txt");
+		String key="0000000000000012";
+		encrypt(key,f,enc);
+		try{
+			Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+			Key key2=new SecretKeySpec( "0000000000000012".getBytes(),ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key2);
+            FileInputStream inputStream=new FileInputStream(enc);
+            byte[] inputBytes=new byte[(int)enc.length()];
+            inputStream.read(inputBytes);
+            
+            byte[] outputBytes=cipher.doFinal(inputBytes);
+            System.out.println(Arrays.toString(outputBytes));
+            System.out.println(new String(outputBytes));
+            inputStream.close();
+         
+		}catch(NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidKeyException | BadPaddingException
+                | IllegalBlockSizeException | IOException ex){ throw new CryptoException("Error encrypting/decrypting file", ex);}
+		
+//		decrypt("0000000000000012",enc,new File("/home/marco/document.decrypted"));
+		
+	}
 }
